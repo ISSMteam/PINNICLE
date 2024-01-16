01894569b3e56c29e77f636475093fa339d2d7de
Cheng Gong <enigne@gmail.com>
2024-01-16 14:16:06 -0500

    add colormap

---
+++ PINN_ICE/utils/plotting.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+import numpy as np
+import math
+
+def cmap_Rignot():
+    """
+    colormap from ISSM
+    """
+    alpha = 1
+    cmap = np.array((np.linspace(0, 1, 128, False), np.ones(128, ), np.ones(128, ))).T
+    cmap[:, 1] = np.maximum(np.minimum((0.1 + cmap[:, 0]**(1 / alpha)), 1), 0)
+    cmap = mpl.colors.hsv_to_rgb(cmap)
+    # construct a colormap object from an array of shape (n, 3 / 4)
+    cmap = ListedColormap(cmap)
+    return cmap
