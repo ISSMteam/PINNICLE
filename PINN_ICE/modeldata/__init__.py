58715cc4e21cd9bfe4416226e338f1e705a74e82
Cheng Gong <enigne@gmail.com>
2024-02-05 22:17:03 -0500

    add log misfit function for vel

--- PINN_ICE/modeldata/__init__.py
+++ PINN_ICE/modeldata/__init__.py
@@ -1,1 +1,2 @@
 from .data import *
+from .data_misfit import surface_log_vel_misfit
--- examples/ssa_Helheim.py
+++ examples/ssa_Helheim.py
@@ -7,12 +7,12 @@ import deepxde as dde
 dde.config.set_default_float('float64')
 dde.config.disable_xla_jit()
 
-weights = [7, 7, 5, 5, 3, 3, 5];
 datestr = datetime.now().strftime("%Y%m%d_%H%M%S")
-inputFileName="Helheim_Weertman_iT080_PINN_fastflow_CF"
+inputFileName="Helheim_fastflow.mat"
 expFileName = "fastflow_CF.exp"
 outputFileName="SSA_2D_test_deepxde"
 modelFolder = "./Models/" + outputFileName + "_" + datestr  + "/"
+
 
 # path for loading data and saving models
 repoPath = "./"
@@ -20,45 +20,44 @@ appDataPath = os.path.join(repoPath, "dataset")
 path = os.path.join(appDataPath, inputFileName)
 # create output folder
 yts =3600*24*365
-loss_weights = [10**(-w) for w in weights]
-loss_weights[2] = loss_weights[2] * yts*yts
-loss_weights[3] = loss_weights[3] * yts*yts
 
 # Hyper parameters
 hp = {}
 # General parameters
-hp["epochs"] = 10000
-hp["loss_weights"] = loss_weights
+hp["epochs"] = 10
 hp["learning_rate"] = 0.001
 hp["loss_function"] = "MSE"
 hp["save_path"] = modelFolder
+hp["is_save"] = False
+hp["is_plot"] = True
 
 # NN
-hp["input_variables"] = ["x","y"]
-hp["output_variables"] = ["u", "v", "s", "H", "C"]
 hp["activation"] = "tanh"
 hp["initializer"] = "Glorot uniform"
 hp["num_neurons"] = 20
 hp["num_layers"] = 6
-hp["output_lb"] = [-200/yts, -5000/yts, -900, 10, 0.01]
-hp["output_ub"] = [7500/yts, 800/yts, 1600, 1700, 8500]
 
 # data
-hp["datasize"] = {"u":4000, "v":4000, "s":4000, "H":4000, "C":None}
+hp["data_size"] = {"u":4000, "v":4000, "s":4000, "H":4000, "C":None}
+hp["data_path"] = path
 
 # domain
 hp["shapefile"] = os.path.join(repoPath, "dataset", expFileName)
 hp["num_collocation_points"] = 9000
 
 # physics
-hp["equations"] = ["SSA"]
-hp["B"] = 1.26802073401e+08
+SSA = {}
+# SSA["input"] = ["x", "y"]
+# SSA["output"] = ["u", "v", "s", "H", "C"]
+# SSA["output_lb"] = [-1.0e4/yts, -1.0e4/yts, -1.0e3,  10.0, 0.01]
+# SSA["output_ub"] = [ 1.0e4/yts,  1.0e4/yts,  2.5e3, 2.0e3, 1.0e4]
+# SSA["data_weights"] = [1.0e-8*yts**2.0, 1.0e-8*yts**2.0, 1.0e-6, 1.0e-6, 1.0e-8]
+# SSA["pde_weights"] = [1.0e-10, 1.0e-10 ]
+SSA["scalar_variables"] = {"B":1.26802073401e+08}
+hp["equations"] = {"SSA": SSA}
 
-# load the data
-X_star, u_star, X_train, u_train, X_bc, u_bc, X_cf, n_cf, uub, ulb, B = \
-pinn.modeldata.prep_2D_data(path, hp["datasize"])
-data = pinn.modeldata.Data(X=X_train, sol=u_train)
-experiment = pinn.PINN(hp, training_data=data)
+# create exp
+experiment = pinn.PINN(hp)
 print(experiment.param)
 experiment.compile()
 experiment.train()
