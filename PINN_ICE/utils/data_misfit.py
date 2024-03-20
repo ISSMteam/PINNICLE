a89f82ffff43c0681d1b701f0f0520fc8d87c9b1
Cheng Gong <enigne@gmail.com>
2024-03-20 10:29:19 -0400

    add MEAN SQUARE LOG

--- PINN_ICE/utils/data_misfit.py
+++ PINN_ICE/utils/data_misfit.py
@@ -14,9 +14,15 @@ def surface_log_vel_misfit(v_true, v_pred):
     epsvel=2.220446049250313e-16
     return bkd.reduce_mean(bkd.square((tf.math.log((tf.abs(v_pred)+epsvel)/(tf.abs(v_true)+epsvel)))))
 
+def mean_squared_log_error(y_true, y_pred):
+    """ use tensorflow function to compute mean squared log error
+    """
+    return tf.keras.losses.MeanSquaredLogarithmicError()(y_true, y_pred)
+
 LOSS_DICT = {
-        "VEL_LOG": surface_log_vel_misfit
-        }
+    "VEL_LOG": surface_log_vel_misfit,
+    "MEAN_SQUARE_LOG": mean_squared_log_error
+    }
 
 def get(identifier):
     """Retrieves a loss function.
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import PINN_ICE as pinn
 import pytest
+import tensorflow as tf
 
 data = {"s":1, "v":[1, 2, 3]}
 
@@ -14,5 +15,13 @@ def test_save_and_load_dict(tmp_path):
 def test_data_misfit():
     with pytest.raises(Exception):
         pinn.utils.data_misfit.get("not defined")
+    assert pinn.utils.data_misfit.get("MSE") != None
     assert pinn.utils.data_misfit.get("VEL_LOG") != None
+    assert pinn.utils.data_misfit.get("MEAN_SQUARE_LOG") != None
     assert pinn.utils.data_misfit.get("MAPE") != None
+
+def test_data_misfit_functions():
+    assert  pinn.utils.data_misfit.get("MSE")(tf.convert_to_tensor([1.0]),tf.convert_to_tensor([1.0])) == 0.0
+    assert  pinn.utils.data_misfit.get("VEL_LOG")(tf.convert_to_tensor([1.0]),tf.convert_to_tensor([1.0])) == 0.0
+    assert  pinn.utils.data_misfit.get("MEAN_SQUARE_LOG")(tf.convert_to_tensor([1.0]),tf.convert_to_tensor([1.0])) == 0.0
+    assert  pinn.utils.data_misfit.get("MAPE")(tf.convert_to_tensor([1.0]),tf.convert_to_tensor([1.0])) == 0.0
