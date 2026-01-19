384a9c3035332500277702cef00dc21474b09e57
enigne <enigne@gmail.com>
2026-01-19 12:47:03 -0500

    bug fix for key error in calving front mini-batch

--- pinnicle/pinn.py
+++ pinnicle/pinn.py
@@ -284,7 +284,7 @@ class PINN:
                 else:
                     if d.lower() == "calvingfront":
                         training_temp.append(dde.icbc.PointSetOperatorBC(training_data.X['nx'], np.zeros_like(training_data.sol['nx']), self.physics.calving_front,
-                                                                         batch_size=min_or_none(self.params.training.mini_batch, training_data.sol[d].shape[0]), shuffle=True))
+                                                                         batch_size=min_or_none(self.params.training.mini_batch, training_data.sol['nx'].shape[0]), shuffle=True))
                         # loss name
                         loss_names.append(self.params.training.additional_loss[d].name)
                         # weights
